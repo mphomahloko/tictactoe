1c5d044f225968e7e582b188358324a9c89c4e7d
Mpho <43447189+mphomahloko@users.noreply.github.com>
2022-12-05 20:33:51 +0200

    using a multidimensional array for the map view

--- src/main/java/com/somesite/GUI.java
+++ src/main/java/com/somesite/GUI.java
@@ -6,62 +6,53 @@ import java.awt.event.ActionListener;
 
 public class GUI extends JFrame
 {
-  JPanel view;
-
-  final JButton a1 = new JButton();
-  final JButton a2 = new JButton();
-  final JButton a3 = new JButton();
+  final JPanel view;
+  final JButton[][] map = new JButton[3][3];
 
 
-  final JButton b1 = new JButton();
-  final JButton b2 = new JButton();
-  final JButton b3 = new JButton();
-
-
-  final JButton c1 = new JButton();
-  final JButton c2 = new JButton();
-  final JButton c3 = new JButton();
   public GUI()
   {
     super("Tic Tac Toe");
+    this.view = new JPanel();
+    for (int rw = 0; rw < 3; rw+=1)
+    {
+      for (int col = 0; col < 3; col+=1)
+      {
+        this.map[rw][col] = new JButton();
+      }
+    }
   }
+
 
   public void init()
   {
-    view = new JPanel();
 
-    view.setLayout(new GridLayout(3, 3));
-    view.add(a1);
-    view.add(a2);
-    view.add(a3);
+    this.view.setLayout(new GridLayout(3, 3));
+    for (int rw = 0; rw < 3; rw+=1)
+    {
+      for (int col = 0; col < 3; col+=1)
+      {
+        this.view.add(this.map[rw][col]);
+      }
+    }
 
-    view.add(b1);
-    view.add(b2);
-    view.add(b3);
 
-    view.add(c1);
-    view.add(c2);
-    view.add(c3);
-
-    this.add(view);
+    this.add(this.view);
     this.setSize(300, 300);
     this.setVisible(true);
     this.setLocationRelativeTo(null);
     this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   }
 
+
   public void  addPlayerInteraction(final ActionListener listener)
   {
-    a1.addActionListener(listener);
-    a2.addActionListener(listener);
-    a3.addActionListener(listener);
-
-    b1.addActionListener(listener);
-    b2.addActionListener(listener);
-    b3.addActionListener(listener);
-
-    c1.addActionListener(listener);
-    c2.addActionListener(listener);
-    c3.addActionListener(listener);
+    for (int rw = 0; rw < 3; rw+=1)
+    {
+      for (int col = 0; col < 3; col+=1)
+      {
+        this.map[rw][col].addActionListener(listener);
+      }
+    }
   }
 }
